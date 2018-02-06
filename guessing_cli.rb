fc0f7f84716fafe943335c910532de450bdf5434
Stacey  Marks <staceyamarks@gmail.com>
2018-02-06 23:10:58 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -8,7 +8,8 @@ def run_guessing_game
     random_number = rand(1..6)
     
     case input 
-      when ""
+      when "exit"
+        break 
       when random_number
         "You guessed the correct number!"
   
