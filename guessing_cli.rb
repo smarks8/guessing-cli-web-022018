e6d9344f82d4fe37ef4d43ff0daddb68645bfc9c
Stacey  Marks <staceyamarks@gmail.com>
2018-02-06 23:12:03 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -14,7 +14,7 @@ def run_guessing_game
       when random_number
         puts "You guessed the correct number!"
       else 
-        puts 
+        puts "The computer guessed #{random_number}."
       end
     end
 end
